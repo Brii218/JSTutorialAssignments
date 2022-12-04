38ba16302150a6f8c0b433c8f3551ec74f769dbb
Sihyang Lee <yansonams2@gmail.com>
2022-12-04 21:21:38 +0900

    sixth commit-test

--- 1-1.1-2.Show an alert with an external script.js
+++ 1-1.1-2.Show an alert with an external script.js
@@ -1,2 +1,2 @@
-// Take the solution of the previous task Show an alert. Modify it by extracting the script content into an external file alert.js, residing in the same folder.
+// Take the solution of the previous task Show an alert. Modify it by extracting the script content into an external file alert.js, residing in the same folder.test
 alert("I'm JavaScript!");
